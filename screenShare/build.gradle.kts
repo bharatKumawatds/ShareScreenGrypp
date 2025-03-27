c68978e523b2e04619d50e75a3a372ed5d5a27d4
bharatKumawatds <108519088+dsgitgetwaizu@users.noreply.github.com>
2025-03-27 16:20:12 +0530

    Testing 1

--- screenShare/build.gradle.kts
+++ screenShare/build.gradle.kts
@@ -35,8 +35,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.opentok.android:opentok-android-sdk:2.29.1")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
